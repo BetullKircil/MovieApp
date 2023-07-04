d4ef79191487efd80eac1ab19f2f0c75698d6d12
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-04 11:01:35 +0300

    [DEV][Betul.Kircil][Developed clean arthitecture layers]

---
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/data/remote/MovieApi.kt
@@ -0,0 +1,5 @@
+package com.betulkircil.movieapp_clean_arthitecture.data.remote
+
+interface MovieApi {
+
+}
---
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/domain/useCase/getMovie/GetMovieUseCase.kt
@@ -0,0 +1,4 @@
+package com.betulkircil.movieapp_clean_arthitecture.domain.useCase.getMovie
+
+class GetMovieUseCase {
+}
