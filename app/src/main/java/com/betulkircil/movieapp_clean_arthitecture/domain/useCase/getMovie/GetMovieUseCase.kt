ac05743dd80f50c36a9ab8eca890f9de00f6a4e8
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-04 11:54:03 +0300

    [DEV][Betul.Kircil][Developed use cases]

--- app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/domain/useCase/getMovie/GetMovieUseCase.kt
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/domain/useCase/getMovie/GetMovieUseCase.kt
@@ -1,4 +1,37 @@
 package com.betulkircil.movieapp_clean_arthitecture.domain.useCase.getMovie
 
-class GetMovieUseCase {
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import com.betulkircil.movieapp_clean_arthitecture.data.remote.dto.toMovieList
+import com.betulkircil.movieapp_clean_arthitecture.domain.model.MovieList
+import com.betulkircil.movieapp_clean_arthitecture.domain.repository.MovieRepository
+import com.betulkircil.movieapp_clean_arthitecture.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOError
+import javax.inject.Inject
+
+class GetMovieUseCase@Inject constructor(private val repository : MovieRepository) {
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMovies(search : String) : Flow<Resource<List<MovieList>>> {
+        return flow {
+            try {
+                emit(Resource.Loading())
+                val movieList = repository.getMovies(search)
+                if(movieList.Response.equals("True")){
+                    emit(Resource.Success(movieList.toMovieList()))
+                }
+                else{
+                    emit(Resource.Error(message = "No movie found"))
+                }
+            }
+            catch (e : IOError){
+                emit(Resource.Error(message = "No internet connection"))
+            }
+            catch (e : HttpException){
+                emit(Resource.Error(message = e.localizedMessage ?: "Error"))
+            }
+        }
+    }
 }
--- app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/domain/useCase/getMovieDetail/GetMovieDetailUseCase.kt
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/domain/useCase/getMovieDetail/GetMovieDetailUseCase.kt
@@ -1,4 +1,32 @@
 package com.betulkircil.movieapp_clean_arthitecture.domain.useCase.getMovieDetail
 
-class GetMovieDetailUseCase {
+import android.net.http.HttpException
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import com.betulkircil.movieapp_clean_arthitecture.data.remote.dto.toMovieDetail
+import com.betulkircil.movieapp_clean_arthitecture.domain.model.MovieDetail
+import com.betulkircil.movieapp_clean_arthitecture.domain.repository.MovieRepository
+import com.betulkircil.movieapp_clean_arthitecture.utils.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import java.io.IOError
+import javax.inject.Inject
+
+class GetMovieDetailUseCase @Inject constructor(private val repository: MovieRepository){
+    @RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+    fun getMovieDeatil(imdbId : String) : Flow<Resource<MovieDetail>> {
+        return flow {
+            try {
+                emit(Resource.Loading())
+                val movieDetail = repository.getMovieDetail(imdbId = imdbId)
+                emit(Resource.Success(movieDetail.toMovieDetail()))
+            }
+            catch (e : IOError){
+                emit(Resource.Error(message = "No internet connection"))
+            }
+            catch (e : HttpException){
+                emit(Resource.Error(message = e.localizedMessage ?: "Error"))
+            }
+        }
+    }
 }
