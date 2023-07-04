68166f46eafcf4b86b76354fdc365e159127801b
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-04 12:00:51 +0300

    [DEV][Betul.Kircil][Provided dagger hilt module and classes]

---
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/Application.kt
@@ -0,0 +1,7 @@
+package com.betulkircil.movieapp_clean_arthitecture
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MovieApplication : Application(){}
