ff60f04705b42e2830f18bc34cfb240bd81050f0
Betül Kırçıl <ceng.betulkircil@gmail.com>
2023-07-04 11:10:45 +0300

    [FIX][Betul.Kircil][Fixed dagger hilt dependency bugs]

---
+++ app/src/main/java/com/betulkircil/movieapp_clean_arthitecture/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.betulkircil.movieapp_clean_arthitecture.utils
+
+object Constants {
+    const val API_KEY = "7f4a7803"
+    const val BASE_URL = "http://www.omdbapi.com/"
+    const val IMDB_ID = "imdb_id"
+}
